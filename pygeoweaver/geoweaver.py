cf56308e7f653d898005e651b933f6c30137e0cb
Gokul Prathin <gokulprathin8@gmail.com>
2023-04-03 11:20:04 -0400

    fix error - ~/ is a directory

--- pygeoweaver/geoweaver.py
+++ pygeoweaver/geoweaver.py
@@ -5,9 +5,11 @@ import subprocess
 def execute_cli_command(command):
     homedir = os.path.expanduser('~')
     jar_path = os.path.join(homedir, 'geoweaver.jar')
+    command_builder = jar_path.split() + command.split()
+    print(f"Executing CLI command {command_builder}")
     # join command
     process = subprocess.Popen(
-        jar_path.split() + command.split(), stdout=subprocess.PIPE
+        command_builder, stdout=subprocess.PIPE
     )
     out, err = process.communicate()
     return out, err
--- pygeoweaver/start.sh
+++ pygeoweaver/start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-wget https://github.com/ESIPFed/Geoweaver/releases/download/latest/geoweaver.jar -O ~/
+wget https://github.com/ESIPFed/Geoweaver/releases/download/latest/geoweaver.jar -O ~/geoweaver.jar
 
 java -jar ~/geoweaver.jar
