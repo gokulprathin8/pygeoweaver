c012e7f5b1b702d322e7232f7ee1bd8abc631c97
Gokul Prathin A <gokulprathin8@gmail.com>
2023-04-16 20:44:59 -0400

    changes to password reset

--- pygeoweaver/sc_resetpassword.py
+++ pygeoweaver/sc_resetpassword.py
@@ -3,11 +3,11 @@ import subprocess
 from pygeoweaver.utils import download_geoweaver_jar, get_geoweaver_jar_path, get_root_dir
 
 
-def reset_password():
+def reset_password(password):
   """
   Usage: <main class> resetpassword
   Reset password for localhost
   """
   download_geoweaver_jar()
-  subprocess.run(["java", "-jar", get_geoweaver_jar_path(), "resetpassword",])
+  subprocess.run(f"java -jar {get_geoweaver_jar_path()} resetpassword -p {password}")
 
